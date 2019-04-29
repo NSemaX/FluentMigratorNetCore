8cd35180407f93c470c80a8565bfde2d8828f77f
NsemaX <nsemax@hotmail.com>
2019-04-29 20:30:35 +0300

    initial commit

---
+++ FluentMigratorTest/Migrations/InitialMigration.cs
@@ -0,0 +1,69 @@
+﻿using FluentMigrator;
+
+namespace FluentMigratorTest.Migrations
+{
+    [Migration(20190418205440, "v1.0.0")] 
+    public class InitialMigration : Migration
+    {
+        public override void Up()
+        {
+            #region Tables
+
+            Create.Table("Players")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Username").AsString(50).NotNullable();
+
+            Create.Table("Cards")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Number").AsString(100).NotNullable()
+                .WithColumn("PlayerId").AsInt32().Nullable();
+
+            Create.Table("Purchases")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Amount").AsDecimal(9, 2).NotNullable()
+                .WithColumn("CardId").AsInt32().NotNullable()
+                .WithColumn("PlayerId").AsInt32().NotNullable();
+            #endregion
+
+            #region Foreign Keys
+            Create.ForeignKey("FK_Cards_PlayerId")
+                .FromTable("Cards").ForeignColumn("PlayerId")
+                .ToTable("Players").PrimaryColumn("Id");
+
+            Create.ForeignKey("FK_Purchases_CardId")
+                .FromTable("Purchases").ForeignColumn("CardId")
+                .ToTable("Cards").PrimaryColumn("Id");
+
+            Create.ForeignKey("FK_Purchases_PlayerId")
+                .FromTable("Purchases").ForeignColumn("PlayerId")
+                .ToTable("Players").PrimaryColumn("Id");
+            #endregion
+
+            #region Initial Data
+
+            //Insert.IntoTable("Players")
+            //    .Row(new { Id = 1, Username = "Player1" })
+            //    .Row(new { Id = 2, Username = "Playe2" });
+            #endregion
+
+        }
+        public override void Down()
+        {
+            #region Foreign Keys
+
+            Delete.ForeignKey("FK_Cards_PlayerId").OnTable("Cards");
+
+            Delete.ForeignKey("FK_Purchases_CardId").OnTable("Purchases");
+            Delete.ForeignKey("FK_Purchases_PlayerId").OnTable("Purchases");
+
+            #endregion
+
+            #region Tables
+            Delete.Table("Players");
+            Delete.Table("Cards");
+            Delete.Table("Purchases");
+            #endregion
+
+        }
+    }
+}
---
+++ FluentMigratorTest/Program.cs
@@ -0,0 +1,72 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+using FluentMigratorTest.Migrations;
+
+namespace FluentMigratorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // add the framework services
+            var services = new ServiceCollection()
+                .AddFluentMigratorCore()
+                // Configure the runner
+                .ConfigureRunner(
+                builder => builder
+                // Use SQL
+                .AddSqlServer2012()
+                // The SQL connection string
+                .WithGlobalConnectionString("Server=.;Database=JokerGames;Trusted_Connection=True;MultipleActiveResultSets=true")
+                // Specify the assembly with the migrations
+                .WithMigrationsIn(typeof(InitialMigration).Assembly))
+                .BuildServiceProvider();
+
+
+
+            // add StructureMap
+            var container = new Container();
+            container.Configure(config =>
+            {
+                // Register stuff in container, using the StructureMap APIs...
+                config.Scan(_ =>
+                {
+                    _.AssemblyContainingType(typeof(Program));
+                    _.WithDefaultConventions();
+                    _.AssemblyContainingType<IMigrationRunnerBuilder>();
+                });
+
+            });
+
+
+
+            try
+            {
+                using (var scope = services.CreateScope())
+                {
+                    // Instantiate the runner
+                    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+                    // Execute the migrations
+                    runner.MigrateUp();
+
+                    //Execute the down scripts
+                    //runner.RollbackToVersion(0);
+
+                    Console.WriteLine("Migration has successfully executed.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
